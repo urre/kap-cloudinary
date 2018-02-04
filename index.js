8ed78c8ddf4d6f811dd57d164fc42033eb17f13d
Urban Sanden <hej@urre.me>
2018-02-04 16:35:28 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const cloudinary = require('cloudinary');
+
+const action = async context => {
+	const filePath = await context.filePath();
+
+	context.setProgress('Uploading…');
+
+	cloudinary.config({
+		cloud_name: context.config.get('cloud_name'),
+		api_key: context.config.get('api_key'),
+		api_secret: context.config.get('api_secret')
+	});
+
+	const upload = cloudinary.uploader.upload(`${filePath}`);
+
+	const response = await upload;
+	context.copyToClipboard(response.secure_url);
+	context.notify('Cloudinary URL copied to the clipboard');
+};
+
+const cloudin = {
+	title: 'Share to Cloudinary',
+	formats: ['gif'],
+	action,
+	config: {
+		cloud_name: {
+			title: 'Cloud Name',
+			type: 'string',
+			default: '',
+			required: true
+		},
+		api_key: {
+			title: 'API Key',
+			type: 'string',
+			default: '',
+			required: true
+		},
+		api_secret: {
+			title: 'API Secret',
+			type: 'string',
+			default: '',
+			required: true
+		}
+	}
+};
+
+exports.shareServices = [cloudin];
---
+++ readme.md
@@ -0,0 +1,18 @@
+# kap-cloudinary [![Build Status](https://travis-ci.org/urre/kap-cloudinary.svg?branch=master)](https://travis-ci.org/urre/kap-cloudinary)
+
+> [Kap](https://github.com/wulkano/kap) plugin - Share on Cloudinary
+
+
+## Install
+
+In the `Kap` menu, go to `Preferences…`, select the `Plugins` pane, find this plugin, and click `Install`.
+
+
+## Usage
+
+In the editor, after recording, select one of the export formats, and then `Share on Cloudinary`.
+
+
+## License
+
+MIT © [Urban Sanden](https://github.com/urre)
